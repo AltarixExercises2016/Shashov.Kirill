40949605e9b2ea979be5661ef8ea567d96c11563
envoy93 <envoy93@gmail.com>
2016-12-13 22:37:37 +0400

    navigation drawer
    update navigation drawer

--- app/src/main/java/com/transportsmr/app/MainActivity.java
+++ app/src/main/java/com/transportsmr/app/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
@@ -22,9 +23,9 @@ import com.transportsmr.app.model.Stop;
 public class MainActivity extends AppCompatActivity implements StopsRecyclerAdapter.StopClickListener {
     public static final String CURRENT_FRAGMENT_KEY = "content";
     public static final String CURRENT_TITLE_KEY = "title";
-    private DrawerLayout dLayout;
     private TransportApp app;
     private Fragment content;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,59 +33,62 @@ public class MainActivity extends AppCompatActivity implements StopsRecyclerAdap
         setContentView(R.layout.activity_main);
 
         app = (TransportApp) getApplication();
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+        //AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setIcon(R.drawable.ic_action_white_hamburger);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                dLayout.openDrawer(GravityCompat.START);
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
             }
         });
 
-        View view = toolbar.getChildAt(1);
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dLayout.openDrawer(GravityCompat.START);
-            }
-        });
+        drawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navView = initNavigationView();
 
         if (savedInstanceState != null) {
             content = getSupportFragmentManager().getFragment(savedInstanceState, CURRENT_FRAGMENT_KEY);
+            openFragment(content, savedInstanceState.containsKey(CURRENT_TITLE_KEY) ? savedInstanceState.getString(CURRENT_TITLE_KEY) : null, false);
+        } else {
+            navView.setCheckedItem(R.id.stops);
+            openFragment(new StopsFragment(), getString(R.string.stops), false);
         }
-
-        setNavigationDrawer();
     }
 
-    private void setNavigationDrawer() {
-        dLayout = (DrawerLayout) findViewById(R.id.navigation_drawer);
+    private NavigationView initNavigationView() {
         NavigationView navView = (NavigationView) findViewById(R.id.navigation);
         navView.setItemIconTintList(null);
         navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
-
+                String title = "";
                 Fragment fragment = null;
                 int itemId = menuItem.getItemId();
 
                 if (itemId == R.id.stops) {
                     fragment = new StopsFragment();
-                    getSupportActionBar().setTitle(getString(R.string.stops));
-                    //Toast.makeText(getApplication(), getPreferences(MODE_PRIVATE).getString("STOPS_LAST_UPDATE", "2"), Toast.LENGTH_LONG).show();
+                    title = getString(R.string.stops);
                 } else if (itemId == R.id.settings) {
                     fragment = new SettingsFragment();
-                    getSupportActionBar().setTitle(getString(R.string.settings));
+                    title = getString(R.string.settings);
                 } else if (itemId == R.id.exit) {
                     app.finish();
                     finish();
                 }
 
                 if (fragment != null) {
-                    openFragment(fragment, false);
-                    dLayout.closeDrawers();
+                    openFragment(fragment, title, false);
+                    drawerLayout.closeDrawers();
                     return true;
                 }
 
@@ -92,33 +96,21 @@ public class MainActivity extends AppCompatActivity implements StopsRecyclerAdap
             }
         });
 
-        //on_startup
-        if (content == null) {
-            navView.setCheckedItem(R.id.stops);
-            openFragment(new StopsFragment(), false);
-            getSupportActionBar().setTitle(getString(R.string.stops));
-        } else {
-            openFragment(content, false); //after activity.destroy
-        }
+        return navView;
     }
 
 
-    private void openFragment(Fragment fragment, boolean isAddToBackStack) {
+    private void openFragment(Fragment fragment, String title, boolean isAddToBackStack) {
         content = fragment;
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, fragment);
+        if (title != null) {
+            getSupportActionBar().setTitle(title);
+        }
         if (isAddToBackStack) {
             transaction.addToBackStack(null);
         }
         transaction.commit();
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        if (savedInstanceState.containsKey(CURRENT_TITLE_KEY)) {
-            getSupportActionBar().setTitle(savedInstanceState.getString(CURRENT_TITLE_KEY));
-        }
     }
 
     @Override
@@ -132,7 +124,15 @@ public class MainActivity extends AppCompatActivity implements StopsRecyclerAdap
 
     @Override
     public void onStopClick(Stop stopDirection) {
-        getSupportActionBar().setTitle(stopDirection.getTitle());
-        openFragment(ArrivalsFragment.newInstance(stopDirection.getKs_id()), true);
+        openFragment(ArrivalsFragment.newInstance(stopDirection.getKs_id()), stopDirection.getTitle(), true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
