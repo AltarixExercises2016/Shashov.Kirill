f1bce91c63937b428083162b0fcd414da3a2ec03
envoy93 <envoy93@gmail.com>
2016-12-16 16:35:32 +0400

    1

--- app/src/main/java/com/transportsmr/app/fragments/ArrivalsFragment.java
+++ app/src/main/java/com/transportsmr/app/fragments/ArrivalsFragment.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,6 +17,7 @@ import android.view.ViewGroup;
 
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import com.github.ivbaranov.mfb.MaterialFavoriteButton;
 import com.transportsmr.app.R;
 import com.transportsmr.app.TransportApp;
 import com.transportsmr.app.adapters.TransportRecyclerAdapter;
@@ -38,6 +40,7 @@ public class ArrivalsFragment extends Fragment {
     private Stop stop;
     private TransportRecyclerAdapter transportAdapter;
     private Context context;
+    private StopDao stopDao;
 
 
     public static ArrivalsFragment newInstance(String stopKsId) {
@@ -61,7 +64,7 @@ public class ArrivalsFragment extends Fragment {
 
         if (getArguments() != null) {
             stopKsId = getArguments().getString(STOP_KS_ID);
-            StopDao stopDao = app.getDaoSession().getStopDao();
+            stopDao = app.getDaoSession().getStopDao();
             stop = stopDao.queryBuilder().where(StopDao.Properties.Ks_id.eq(stopKsId)).list().get(0);
         }
     }
@@ -78,19 +81,26 @@ public class ArrivalsFragment extends Fragment {
         // Inflate the layout for this fragment
         LinearLayout view = (LinearLayout) inflater.inflate(R.layout.fragment_arrivals, container, false);
 
+        TextView stopTitle = (TextView) view.findViewById(R.id.stop_direction_title);
+        stopTitle.setText(stop.getTitle());
         TextView stopStreet = (TextView) view.findViewById(R.id.stop_direction_street);
         stopStreet.setText(stop.getAdjacentStreet());
         TextView stopDirection = (TextView) view.findViewById(R.id.stop_direction_direction);
         stopDirection.setText(stop.getDirection());
+        MaterialFavoriteButton fav = (MaterialFavoriteButton) view.findViewById(R.id.stop_direction_favorite);
+        fav.setFavorite(stop.getFavorite(), false);
+        fav.setOnFavoriteChangeListener(new MaterialFavoriteButton.OnFavoriteChangeListener() {
+            @Override
+            public void onFavoriteChanged(MaterialFavoriteButton materialFavoriteButton, boolean b) {
+                stop.setFavorite(b);
+                stopDao.update(stop);
+            }
+        });
 
         final RecyclerView transportRecycler = (RecyclerView) view.findViewById(R.id.rvItems);
-
-        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.refresh);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(context);
-        transportRecycler.setLayoutManager(layoutManager);
-
+        transportRecycler.setLayoutManager(new LinearLayoutManager(context));
         transportRecycler.setAdapter(transportAdapter);
-
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.refresh);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
